22ac897cedaede0d8ad8c47b8a12e1c0a4a290ff
Kesnel Samuel <kesnelsamuel@gmail.com>
2023-07-24 11:06:10 -0400

    LSldsf

--- courses/models.py
+++ courses/models.py
@@ -143,8 +143,8 @@ class Courses(models.Model):
     introduction    =   models.TextField(null=True, blank=True, verbose_name="Introduction", help_text="Write in some lines more info about this course")
     description     =   tinymce_models.HTMLField(null=True, blank=False, verbose_name="Description", help_text="Start writing about this course...")
 
-    location        =   models.CharField(max_length=200, null=True, blank=False, verbose_name='Location', unique=False)
-    location_map    =   models.TextField(null=True, blank=False, verbose_name='Location map', unique=False, help_text="Insert the google map iFrame code")
+    location        =   models.CharField(max_length=200, null=True, blank=True, verbose_name='Location')
+    location_map    =   models.TextField(null=True, blank=True, verbose_name='Location map', help_text="Insert the google map iFrame code")
 
     price           =   models.IntegerField(null=True, blank=True, verbose_name="Price", help_text="Price in HTG")
 
